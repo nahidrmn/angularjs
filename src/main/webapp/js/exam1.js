e2bbec8c5c95f845e1777f7f821fba1e87879ed8
Md Nahid Rahman <nahidrmn@gmail.com>
2016-04-06 14:13:58 -0700

    Angular jadoo...

---
+++ src/main/webapp/js/exam1.js
@@ -0,0 +1,17 @@
+var app1 = angular.module('app1',[]);
+
+app1.controller('ctrl1',function($scope){
+	
+	$scope.first=1;
+	$scope.second=2;
+	
+	$scope.updateValue = function(){
+		$scope.calculation=$scope.first+'+'+$scope.second+'='+(+$scope.first + +$scope.second);
+	};
+	
+});
+
+app1.controller('ctrl2',function($scope){
+	var moods = ["disregard","happy","sad","terrific"];
+	$scope.feelings=moods[Math.floor((Math.random()*4))];
+});
