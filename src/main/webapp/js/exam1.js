a6c2156d06778b00b72bc1649f1d4caa525a8291
Md Nahid Rahman <nahidrmn@gmail.com>
2016-04-07 15:22:57 -0700

    Angular JS Practice One

--- src/main/webapp/js/exam1.js
+++ src/main/webapp/js/exam1.js
@@ -1,17 +1,33 @@
 var app1 = angular.module('app1',[]);
 
 app1.controller('ctrl1',function($scope){
-	
 	$scope.first=1;
 	$scope.second=2;
-	
 	$scope.updateValue = function(){
 		$scope.calculation=$scope.first+'+'+$scope.second+'='+(+$scope.first + +$scope.second);
 	};
-	
 });
 
 app1.controller('ctrl2',function($scope){
 	var moods = ["disregard","happy","sad","terrific"];
 	$scope.feelings=moods[Math.floor((Math.random()*4))];
+});
+
+app1.controller('ctrl3',function($scope){
+	$scope.groceries = [
+	                   {item:"tomatoes",purchased:false},
+	                   {item:"potatoes",purchased:true},
+	                   {item:"hummus",purchased:true},
+	                   {item:"salad",purchased:false}
+	                  ];
+	$scope.getList = function(){
+		return $scope.showList ? "uGroceryList.html" : "oGroceryList.html";
+	};
+	$scope.blur=0;
+	$scope.keydown = function(e){
+		$scope.kdKey = String.fromCharCode(e.keyCode);
+	};
+	$scope.disableButton=true;
+	$scope.dayTimeButton=true;
+	
 });
